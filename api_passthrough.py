6082bb4261adcf890fb891811e89f634502362c9
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2023-08-01 15:05:56 -0400

    Update api_passthrough.py
    * Adding support for Resource/ID GETs

--- api_passthrough.py
+++ api_passthrough.py
@@ -12,6 +12,7 @@ from fhir.resources.R4B.bundle import Bundle
 
 from models import JWKS
 from util import fhir_auth, fhir_url
+from helpers import check_response
 
 logger: logging.Logger = logging.getLogger('main.api_passthrough')
 
@@ -40,20 +41,53 @@ def return_jwks() -> JWKS:
     return JWKS(**jwks_obj)
 
 
+@api_passthrough_router.get('/{resource_type}/{id}', response_model=dict)
+def return_resource_by_id(resource_type: str, id: str) -> OperationOutcome | dict:
+    '''Function for reading a resource given its id'''
+
+    start_time = time.time()
+    logger.info(f'Reading {resource_type} Resource with ID: {id}')
+    query_headers = {'Accept': 'application/json'}
+
+    if fhir_auth:
+        query_headers['Authorization'] = fhir_auth
+
+    resource_read = requests.get(fhir_url+f'{resource_type}/{id}', headers=query_headers)
+
+    check_output: OperationOutcome | None = check_response(resource_type=resource_type, resp=resource_read)
+    if check_output:
+        return check_output
+
+    try:
+        logger.debug(f'External call took {round(resource_read.elapsed.total_seconds(), 4)} seconds')
+        logger.debug(f'This call took {round(time.time() - start_time, 4)} seconds')
+        return resource_read.json()
+    except requests.exceptions.JSONDecodeError:
+        logger.error(f'Status Code: {resource_read.status_code}')
+        logger.error(f'Response Text: {resource_read.text}')
+        return OperationOutcome(issue=[{'severity': 'error',
+                                        'code': 'processing',
+                                        'diagnostics': 'The response returned from the FHIR_URL was not JSON parseable, please see logs for what the server responded'}]) # type: ignore
+
+
 @api_passthrough_router.get('/{resource_type}', response_model_exclude_none=True)
 def return_resource(resource_type: str, req: Request) -> OperationOutcome | Bundle | None:
 
     start_time = time.time()
     search_params = dict(req.query_params)
     query_string = resource_type+'?'+req.url.query
+
     logger.info(f'Searching {resource_type} with Parameters: {search_params}')
     query_headers = {'Accept': 'application/json'}
 
     if fhir_auth:
         query_headers['Authorization'] = fhir_auth
-        resp = requests.get(fhir_url+query_string, headers=query_headers)
-    else:
-        resp = requests.get(fhir_url+query_string, headers=query_headers)
+
+    resp = requests.get(fhir_url+query_string, headers=query_headers)
+
+    check_output: OperationOutcome | None = check_response(resource_type=resource_type, resp=resp)
+    if check_output:
+        return check_output
 
     try:
         logger.debug(f'External call took {round(resp.elapsed.total_seconds(), 4)} seconds')
